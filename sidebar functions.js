347d1e0bf080d474f4faaba8de3826eef77fe414
John Phonxaya <phonxayajohn@gmail.com>
2023-06-06 12:12:48 -0500

    removed redunant HTML for address, added event listener for contactDetails

--- sidebar functions.js
+++ sidebar functions.js
@@ -6,14 +6,26 @@ function renderContacts(contacts) {
 
 function renderContact(contact) {
     const card = document.createElement('div')
-    card.classList.add(/* css class */)
-    const cardId = `card-${contact.id}`
+    card.classList.add('contact-card')
     card.innerHTML = `
     <p1 id="name">${contact.name}</p1>
     <img id="contact-photo" src="${contact.photo}" alt=${contact.name} />
     `
-    // .append(card)
+    document.getElementById('contact-list').append(card)
 
-    // card.addEventListener('click', () => contactDetails(contact))
+    card.addEventListener('click', () => contactDetails(contact))
 }
 
+function contactDetails(contact) {
+    const contactDetail = document.getElementById('contact-info')
+    const detailImage = contactDetail.querySelector('.detail-image')
+
+    detailImage.src = contact.photo
+    detailImage.alt = contact.name
+
+    contactDetail.querySelector('#name-display').textContent = contact.name
+    contactDetail.querySelector('#phone-display').textContent = contact.phone
+    contactDetail.querySelector('#email-display').textContent = contact.email
+    contactDetail.querySelector('#address-display').textContent = `${contact.address}, ${contact.state}`
+
+}
